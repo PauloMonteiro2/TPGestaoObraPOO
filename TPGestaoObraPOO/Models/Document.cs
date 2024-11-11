4d22a40a50b4e6d6a432ed3eb8873ccfef1866c7
PauloMonteiro2 <a25460@alunos.ipca.pt>
2024-11-11 19:26:16 +0000

    Class_Document

---
+++ TPGestaoObraPOO/Models/Document.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TPGestaoDeObraPOO.Moldels
+{
+    public class Document
+    {
+
+        #region Public Properties
+        public int DocumentId { get; private set; }
+        public DocumentType DocumentType { get; set; }
+        public DateOnly DocumentDate { get; private set; }
+        public Person Seller { get; set; }
+        #endregion
+
+        #region Constructor
+        public Documents(int documentId, DocumentType documentType, DateOnly documentDate, Person seller)
+        {
+            this.DocumentId = documentId;
+            this.DocumentType = documentType;
+            this.DocumentDate = documentDate;
+            this.Seller = seller;
+        }
+        #endregion
+    }
+}
+
